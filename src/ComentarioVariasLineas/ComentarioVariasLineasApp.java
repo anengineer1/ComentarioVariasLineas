b08d962a0f94dc4a257b7b2b466f1f573b294c4f
yamir <yamir@mandragora.mandragora>
2023-03-07 20:24:34 +0100

    Corrección comment múltiples líneas

--- src/ComentarioVariasLineas/ComentarioVariasLineasApp.java
+++ src/ComentarioVariasLineas/ComentarioVariasLineasApp.java
@@ -3,9 +3,9 @@ package ComentarioVariasLineas;
 public class ComentarioVariasLineasApp {
 
 	public static void main(String[] args) {
-			// System.out.println("Hola");
-			// System.out.println("Adios");
-			// System.out.println("FIN DEL PROGRAMA");
+			/* System.out.println("Hola");
+			System.out.println("Adios");
+			System.out.println("FIN DEL PROGRAMA"); */
 			for (int i = 0; i < 6; i++)
 			    System.out.print(i+" ");
 			System.out.println("FIN DEL PROGRAMA");
