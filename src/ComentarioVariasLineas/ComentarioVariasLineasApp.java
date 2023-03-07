e524cbb9bcc2fcfbf2667c785d33e3eec91f1f63
yamir <yamir@mandragora.mandragora>
2023-03-07 12:24:51 +0100

    First commit

---
+++ src/ComentarioVariasLineas/ComentarioVariasLineasApp.java
@@ -0,0 +1,13 @@
+package ComentarioVariasLineas;
+
+public class ComentarioVariasLineasApp {
+
+	public static void main(String[] args) {
+			// System.out.println("Hola");
+			// System.out.println("Adios");
+			// System.out.println("FIN DEL PROGRAMA");
+			for (int i = 0; i < 6; i++)
+			    System.out.print(i+" ");
+			System.out.println("FIN DEL PROGRAMA");
+	}
+}
